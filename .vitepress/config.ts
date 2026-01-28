e116efbf70a343b92d105797691d226a7d9a0c06
indaco <indaco@users.noreply.github.com>
2026-01-28 14:38:15 +0100

    fix: editLink on vitepress config

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -54,7 +54,7 @@ export default defineConfig({
 
     // Edit link
     editLink: {
-      pattern: 'https://github.com/indaco/sley-docs/edit/main/docs/:path',
+      pattern: 'https://github.com/indaco/sley-docs/edit/main/pages/:path',
       text: 'Edit this page on GitHub',
     },
 
